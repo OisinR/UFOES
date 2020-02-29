f727756ba9d3bca7130e99565a3b24f9d2954328
oisintheginger <32101314+oisintheginger@users.noreply.github.com>
2020-02-29 16:46:34 +0000

    Yargh

---
+++ U.F.Oes/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] float minY, maxY, minX, maxX, minZ, maxZ, moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 newPos =Vector3.Lerp(transform.position, transform.position + (Input.GetAxis("Horizontal") * Vector3.right) +
+                                                (Input.GetAxis("Vertical") * Vector3.forward) +
+                                                (-Input.mouseScrollDelta.y * Vector3.up * 4f), moveSpeed);
+        
+        if(newPos.y<minY)
+        {
+            newPos.y = minY;
+        }
+        if (newPos.y > maxY)
+        {
+            newPos.y = maxY;
+        }
+        if (newPos.x < minX)
+        {
+            newPos.x = minX;
+        }
+        if (newPos.x > maxX)
+        {
+            newPos.x = maxX;
+        }
+        if (newPos.z < minZ)
+        {
+            newPos.z = minZ;
+        }
+        if (newPos.z > maxZ)
+        {
+            newPos.z = maxZ;
+        }
+        transform.position = newPos;
+    }
+}
---
+++ U.F.Oes/Assets/Scripts/ControlScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlScript : MonoBehaviour
+{
+    [SerializeField] GameObject Alien1, Alien2, Alien3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit rH;
+        Ray moveRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+       if(Input.GetMouseButtonDown(0))
+        {
+            if(Physics.Raycast(moveRay, out rH))
+            {
+                Alien1.GetComponent<AlienMovement>().thisAlienAgent.SetDestination(rH.point);
+            }
+
+
+        }
+    }
+}
