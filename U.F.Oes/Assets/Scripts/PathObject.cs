02a0b0d9370231a25a822b5627ca464c5c022a2a
oisintheginger <32101314+oisintheginger@users.noreply.github.com>
2020-03-02 18:14:07 +0000

    Human Paths and Path Switching

--- U.F.Oes/Assets/Scripts/PathObject.cs
+++ U.F.Oes/Assets/Scripts/PathObject.cs
@@ -5,15 +5,6 @@ using UnityEngine;
 public class PathObject : MonoBehaviour
 {
     public List<Transform> pathPoints;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
