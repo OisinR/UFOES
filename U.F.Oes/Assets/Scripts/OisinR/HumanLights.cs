c0bac3828623ab1c7484a7d19667a669b9eef2c3
Oisin <C17343216@mydit.ie>
2020-03-10 16:35:36 +0000

    Lighting working

---
+++ U.F.Oes/Assets/Scripts/OisinR/HumanLights.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanLights : MonoBehaviour
+{
+
+    public LightItUp Lighting;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Human")
+        {
+            Lighting.LightSwitch = true;
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Human")
+        {
+            Lighting.LightSwitch = false;
+        }
+    }
+}
