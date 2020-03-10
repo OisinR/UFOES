052117b2f61e9446f941e767962990226a34960b
Oisin <C17343216@mydit.ie>
2020-03-10 15:31:17 +0000

    Garden Lights

---
+++ U.F.Oes/Assets/Scripts/OisinR/LightItUp.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightItUp : MonoBehaviour
+{
+
+    public bool LightSwitch;
+    public GameObject[] lights;
+    public Material[] materials;
+
+    void Update()
+    {
+        if(LightSwitch)
+        {
+            foreach(GameObject g in lights)
+            {
+                g.SetActive(true);
+            }
+            foreach(Material m in materials)
+            {
+                m.EnableKeyword("_EMISSION");
+            }
+        }
+        else
+        {
+            foreach (GameObject g in lights)
+            {
+                g.SetActive(false);
+            }
+            foreach (Material m in materials)
+            {
+                m.DisableKeyword("_EMISSION");
+            }
+        }
+    }
+}
