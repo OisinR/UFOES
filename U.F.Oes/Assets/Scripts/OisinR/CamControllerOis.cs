972b6edfc0d82686f8c6da3e9f5a6a0e8267c1c3
Oisin <C17343216@mydit.ie>
2020-03-12 02:12:03 +0000

    Camera Updates

--- U.F.Oes/Assets/Scripts/OisinR/CamControllerOis.cs
+++ U.F.Oes/Assets/Scripts/OisinR/CamControllerOis.cs
@@ -5,18 +5,26 @@ using UnityEngine;
 public class CamControllerOis : MonoBehaviour
 {
     public static GameObject target;
-
+    public GameObject[] targets;
     public  float minY, maxY, minX, maxX, minZ, maxZ, moveSpeed;
-
-
+    bool changedAlien;
+    int currentAlien;
+    float counter;
     /*
      *  -x to go up + x to go down          : z
      *  -z to go left + z to go right       : x
      *  +y go upVert + -y to go downVert    : y
      */
-    
+
+    private void Start()
+    {
+        currentAlien = 0;
+    }
+
     void Update()
     {
+
+        /*
         //up and down
         minX = target.transform.position.x - 20;
         maxX = target.transform.position.x + 20;
@@ -28,6 +36,13 @@ public class CamControllerOis : MonoBehaviour
         //scroll
         minY = target.transform.position.y + 10;
         maxY = target.transform.position.y + 80;
+        */
+        
+        if(changedAlien)
+        {
+            ChangeAlien(currentAlien);
+
+        }
 
         Vector3 newPos = Vector3.Lerp(transform.position, transform.position + (Input.GetAxis("Horizontal") * Vector3.forward) + (Input.GetAxis("Vertical") * Vector3.left) +
                                         (-Input.mouseScrollDelta.y * Vector3.up * 4f), moveSpeed);
@@ -58,5 +73,29 @@ public class CamControllerOis : MonoBehaviour
             newPos.z = maxZ;
         }
         transform.position = newPos;
+    }
+
+
+
+    void ChangeAlien(int i)
+    {
+        changedAlien = true;
+        transform.position = Vector3.Slerp(transform.position,new Vector3 (targets[i].transform.position.x,transform.position.y-0.01f, targets[i].transform.position.z), 0.1f);
+        if(counter > 0)
+        {
+            counter -= Time.deltaTime;
+        }
+        else
+        {
+            changedAlien = false;
+        }
+
+    }
+
+    public void Button(int i)
+    {
+        counter = 0.5f;
+        currentAlien = i;
+        ChangeAlien(i);
     }
 }
